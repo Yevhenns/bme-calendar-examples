03bdee1fa80c7a1d729d0964f85f5c257c4e9b56
Yevhenns <gifestum@gmail.com>
2025-03-27 11:47:28 +0200

    added for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import "./App.css";
 function App() {
   const [day, setDay] = useState<string>();
   const [range, setRange] = useState<string[]>();
+  const [day1, setDay1] = useState<string>();
+  const [range1, setRange1] = useState<string[]>();
 
   return (
     <>
@@ -19,6 +21,26 @@ function App() {
         <p>Список дат:</p>
         <ul>
           {range?.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+      </div>
+
+      <h2>Розмір 40х32px</h2>
+      <h2>Календар</h2>
+      <Calendar setDay={setDay1} daySize={{ width: 40, height: 32 }} />
+      <p>Дата: {day1}</p>
+      <h2>Діапазон дат</h2>
+      <Calendar
+        type="range"
+        setRange={setRange1}
+        range={range1}
+        daySize={{ width: 40, height: 32 }}
+      />
+      <div>
+        <p>Список дат:</p>
+        <ul>
+          {range1?.map((item) => {
             return <li key={item}>{item}</li>;
           })}
         </ul>
